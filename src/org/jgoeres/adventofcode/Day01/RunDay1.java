e247611bc388e298d58f6915ed7e6bb6520f38fa
Justin Goeres <justin.goeres@gmail.com>
2018-12-08 21:10:10 -0500

    Day 1: Refactored 1A & 1B into separate Day1 class, and Main to call them.

---
+++ src/org/jgoeres/adventofcode/Day01/RunDay1.java
@@ -0,0 +1,51 @@
+package org.jgoeres.adventofcode.Day01;
+
+public abstract class RunDay1 {
+    static String pathToInputs = "day01/input.txt";
+    static FrequencyChangeService frequencyChangeService = new FrequencyChangeService(pathToInputs);
+
+    static Integer totalFrequencyChange = null;
+
+    public static void problem1A() {
+        // Day 01A
+        // Starting with a frequency of zero, what is the resulting frequency
+        // after all of the changes in frequency have been applied?
+        System.out.println("=== DAY 1A ===");
+
+        try {
+            totalFrequencyChange = frequencyChangeService.calculateTotalFrequencyChange(0);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        System.out.println("Day 1A: Total Frequency Change = " + totalFrequencyChange);
+        // Answer: 437
+    }
+
+    public static void problem1B() {
+        // Day 01B
+        // You notice that the device repeats the same frequency change list over and over.
+        // To calibrate the device, you need to find the first frequency it reaches twice.
+        System.out.println("=== DAY 1B ===");
+
+        // This MUST run after problem1A() above, or the frequencyHistogram in
+        // frequencyChangeService won't be initialized.
+        System.out.println("Scanning for duplicates...");
+        System.out.print("Pass: ");
+        int pass = 1;
+        while (true) {
+//            System.out.println("Pass at totalFrequencyChange = " + totalFrequencyChange);
+            System.out.print(pass);
+            try {
+                totalFrequencyChange = frequencyChangeService.calculateTotalFrequencyChange(totalFrequencyChange);
+            } catch (Exception e) {
+                System.out.println("\nException: " + e.getMessage());
+
+                break;
+            }
+            System.out.print(", ");
+            pass = pass + 1;
+        }
+        // Answer: 655 after 138 passes
+    }
+}
--- src/org/jgoeres/adventofcode/Main.java
+++ src/org/jgoeres/adventofcode/Main.java
@@ -1,51 +1,21 @@
 package org.jgoeres.adventofcode;
 
-import org.jgoeres.adventofcode.Day01.FrequencyChangeService;
+import org.jgoeres.adventofcode.Day01.RunDay1;
 
 public class Main {
 
     public static void main(String[] args) {
-        // Day 01
-        //https://adventofcode.com/2018/day/1
+        //https://adventofcode.com/2018/
 
         // Day 01A
         // Starting with a frequency of zero, what is the resulting frequency
         // after all of the changes in frequency have been applied?
-        System.out.println("=== DAY 1A ===");
-
-        String pathToInputs = "day01/input.txt";
-        FrequencyChangeService frequencyChangeService = new FrequencyChangeService(pathToInputs);
-
-        Integer totalFrequencyChange = null;
-        try {
-            totalFrequencyChange = frequencyChangeService.calculateTotalFrequencyChange(0);
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
-
-        System.out.println("Day 1A: Total Frequency Change = " + totalFrequencyChange);
-        // Answer: 437
+        RunDay1.problem1A();
 
         // Day 01B
         // You notice that the device repeats the same frequency change list over and over.
         // To calibrate the device, you need to find the first frequency it reaches twice.
-        System.out.println("=== DAY 1B ===");
+        RunDay1.problem1B();
 
-        System.out.println("Scanning for duplicates...");
-        System.out.print("Pass: ");
-        int pass = 1;
-        while (true) {
-//            System.out.println("Pass at totalFrequencyChange = " + totalFrequencyChange);
-            System.out.print(pass);
-            try {
-                totalFrequencyChange = frequencyChangeService.calculateTotalFrequencyChange(totalFrequencyChange);
-            } catch (Exception e) {
-                System.out.println("\nException: " + e.getMessage());
-
-                break;
-            }
-            System.out.print(", ");
-            pass = pass + 1;
-        }
     }
 }
