c247d6606e49d7dd69d37e248e4c7bdf84012769
Justin Goeres <justin.goeres@gmail.com>
2018-12-08 20:45:21 -0500

    First implementation of Day 1B

--- src/org/jgoeres/adventofcode/Day01/FrequencyChangeService.java
+++ src/org/jgoeres/adventofcode/Day01/FrequencyChangeService.java
@@ -2,20 +2,77 @@ package org.jgoeres.adventofcode.Day01;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class FrequencyChangeService {
-    public int calculateFrequencyChangeFromFile(String pathToFile) {
-        int totalFrequencyChange = 0;
 
-        try (BufferedReader br = new BufferedReader(new FileReader(pathToFile))) {
-            String line;
-            while ((line = br.readLine()) != null) {
-                // process the line.
-                totalFrequencyChange = totalFrequencyChange + Integer.parseInt(line);
+    public int minFrequency = 0;
+    public int maxFrequency = 0;
+
+    public ArrayList<Integer> frequencyHistory = new ArrayList<>();
+    public HashMap<Integer, Integer> frequencyHistogram = new HashMap<>();
+
+    public FrequencyChangeService(String pathToFile) {
+        loadFrequencyList(pathToFile);
+    }
+
+    public Integer calculateTotalFrequencyChange(int baseFrequency) {
+        int totalFrequencyChange = baseFrequency;
+
+        for (Integer thisFrequency : frequencyHistory) {
+            totalFrequencyChange = totalFrequencyChange + thisFrequency;
+            if (frequencyHistogram.containsKey(totalFrequencyChange)) {
+                // Found a duplicate frequency!
+                System.out.println("Duplicate frequency found: " + totalFrequencyChange);
+                return null;
+            } else {
+                frequencyHistogram.put(totalFrequencyChange,1);
             }
-        } catch (Exception e) {
-            System.out.println("Exception occurred: " + e.getMessage());
         }
         return totalFrequencyChange;
     }
+
+    private void loadFrequencyList(String pathToFile) {
+        frequencyHistory.clear();
+//        frequencyHistory.add(0);
+        try (BufferedReader br = new BufferedReader(new FileReader(pathToFile))) {
+            String line;
+            Integer nextFrequency = 0;
+            while ((line = br.readLine()) != null) {
+                // process the line.
+                nextFrequency = Integer.parseInt(line);
+
+                frequencyHistory.add(nextFrequency);
+            }
+        } catch (
+                Exception e)
+
+        {
+            System.out.println("Exception occurred: " + e.getMessage());
+        }
+    }
+
+    public Integer findDuplicate(int frequencyIncrement) {
+        for (int nextHistoryFrequency : getFrequencyHistory()) {
+            int frequencyToCheck = nextHistoryFrequency + frequencyIncrement;
+            if (getFrequencyHistory().contains(frequencyToCheck)) {
+                return frequencyToCheck; // Found a duplicate!
+            }
+        }
+        return null; // No duplicate found, return null.
+    }
+
+    public int getMinFrequency() {
+        return minFrequency;
+    }
+
+    public int getMaxFrequency() {
+        return maxFrequency;
+    }
+
+    private ArrayList<Integer> getFrequencyHistory() {
+        return frequencyHistory;
+    }
+
 }
--- src/org/jgoeres/adventofcode/Main.java
+++ src/org/jgoeres/adventofcode/Main.java
@@ -5,20 +5,44 @@ import org.jgoeres.adventofcode.Day01.FrequencyChangeService;
 public class Main {
 
     public static void main(String[] args) {
-    	// Day 01
+        // Day 01
         //https://adventofcode.com/2018/day/1
 
-        //Starting with a frequency of zero, what is the resulting frequency
+        // Day 01A
+        // Starting with a frequency of zero, what is the resulting frequency
         // after all of the changes in frequency have been applied?
 
-        FrequencyChangeService frequencyChangeService = new FrequencyChangeService();
 
         String pathToInputs = "day01/input.txt";
+        FrequencyChangeService frequencyChangeService = new FrequencyChangeService(pathToInputs);
 
-        int totalFrequency = 0;
-        totalFrequency += frequencyChangeService.calculateFrequencyChangeFromFile(pathToInputs);
+        Integer totalFrequencyChange = frequencyChangeService.calculateTotalFrequencyChange(0);
 
-        System.out.println("Day 1: Total Frequency Change = " + totalFrequency);
+        System.out.println("Day 1A: Total Frequency Change = " + totalFrequencyChange);
         // Answer: 437
+
+        System.out.println("Min: " + frequencyChangeService.getMinFrequency());
+        System.out.println("Max: " + frequencyChangeService.getMaxFrequency());
+        System.out.println("Width: " +
+                (frequencyChangeService.getMaxFrequency() - frequencyChangeService.getMinFrequency()));
+
+        // Day 02
+        // You notice that the device repeats the same frequency change list over and over.
+        // To calibrate the device, you need to find the first frequency it reaches twice.
+
+        // If the total frequency change for each pass through the list is +totalFrequencyChange,
+        // then each successive pass is +totalFrequency *above* the previous one.
+
+        // The first frequency match has to match against the FIRST set of frequencies,
+        // so we can just iteratively add +totalFrequencyChange to the frequencyHistory
+        // and check point-by-point to see if each frequency exists in the original frequencyHistory
+
+
+        boolean keepGoing = true;
+        while (keepGoing) {
+            System.out.println("Pass at totalFrequencyChange = " + totalFrequencyChange);
+            totalFrequencyChange = frequencyChangeService.calculateTotalFrequencyChange(totalFrequencyChange);
+            keepGoing = (totalFrequencyChange != null);
+        }
     }
 }
