35a05758d3bdc335b667960986073c3aae13cea9
Justin Goeres <jgoeres@liveareacx.com>
2018-12-13 14:43:48 -0500

    Day 6B Implementation complete.

--- src/org/jgoeres/adventofcode/Day06/RunDay6.java
+++ src/org/jgoeres/adventofcode/Day06/RunDay6.java
@@ -68,9 +68,77 @@ public class RunDay6 {
 
     public static void problem6B() {
         /*
-        Problem Description
+        On the other hand, if the coordinates are safe, maybe the best you can do
+        is try to find a region near as many coordinates as possible.
+
+        For example, suppose you want the sum of the Manhattan distance to all of
+        the coordinates to be less than 32. For each location, add up the distances
+        to all of the given coordinates; if the total of those distances is less
+        than 32, that location is within the desired region. Using the same coordinates
+        as above, the resulting region looks like this:
+
+        ..........
+        .A........
+        ..........
+        ...###..C.
+        ..#D###...
+        ..###E#...
+        .B.###....
+        ..........
+        ..........
+        ........F.
+
+        In particular, consider the highlighted location 4,3 located at the
+        top middle of the region. Its calculation is as follows, where abs()
+        is the absolute value function:
+
+        Distance to coordinate A: abs(4-1) + abs(3-1) =  5
+        Distance to coordinate B: abs(4-1) + abs(3-6) =  6
+        Distance to coordinate C: abs(4-8) + abs(3-3) =  4
+        Distance to coordinate D: abs(4-3) + abs(3-4) =  2
+        Distance to coordinate E: abs(4-5) + abs(3-5) =  3
+        Distance to coordinate F: abs(4-8) + abs(3-9) = 10
+        Total distance: 5 + 6 + 4 + 2 + 3 + 10 = 30
+
+        Because the total distance to all coordinates (30) is less than 32,
+        the location is within the region.
+
+        This region, which also includes coordinates D and E, has a total size
+        of 16.
+
+        Your actual region will need to be much larger than this example,
+        though, instead including all locations with a total distance of less
+        than 10000.
+
+        What is the size of the region containing all locations which have a
+        total distance to all given coordinates of less than 10000?
         */
         System.out.println("=== DAY 6B ===");
 
+        Centers centers = new Centers(pathToInputs);
+        GridBoundary gridBoundary = GridService.findGridBoundary(centers);
+
+        // Initialize a grid to hold everything.
+        Grid grid = new Grid(gridBoundary);
+
+        Integer numClosePoints = 0;
+        Integer closeDistance = 10000;
+        
+        // Iterate through the whole grid, adding up all the distances for each point.
+        for (GridPoint gridPoint : grid.getGridPoints()) {
+            Integer totalDistance = 0;
+            for (GridPoint center : centers.getGridPoints()) {
+                // calculate the distance to every center point.
+                totalDistance += GridService.manhattanDistance(gridPoint, center);
+            }
+            if (totalDistance < closeDistance) {
+                // If this is a "close point" add it to the running total.
+                numClosePoints = numClosePoints + 1;
+            }
+        }
+        System.out.println("Number of points closer than " + closeDistance + " to all centers: " + numClosePoints);
+
+        // Answer:
+        // Number of points closer than 10000 to all centers: 35334
     }
 }
