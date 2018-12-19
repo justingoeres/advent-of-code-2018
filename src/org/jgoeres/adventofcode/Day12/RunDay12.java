b814c67719d24e2875721a7642e3d0775c66dff6
Justin Goeres <justin.goeres@gmail.com>
2018-12-18 23:35:17 -0500

    Day 12B initial checkin

--- src/org/jgoeres/adventofcode/Day12/RunDay12.java
+++ src/org/jgoeres/adventofcode/Day12/RunDay12.java
@@ -26,21 +26,21 @@ public class RunDay12 {
         ArrayList<Rule> rules = greenhouse.getRules(); // use the greenhouse's rules.
 
         if (DEBUG_PRINT_GREENHOUSE) {
-            System.out.println("S:\t" + addSpaces(currentPots) + printPots(currentPots));
+            System.out.println("S:\t" + addSpaces(currentPots, GENERATIONS_TO_RUN) + printPots(currentPots));
         }
 
         for (int generation = 1; generation <= GENERATIONS_TO_RUN; generation++) {
             currentPots = GreenhouseService.calculateNextPots(currentPots, rules);
 
             if (DEBUG_PRINT_GREENHOUSE) {
-                System.out.println(generation + ":\t" + addSpaces(currentPots) + printPots(currentPots));
+                System.out.println(generation + ":\t" + addSpaces(currentPots, GENERATIONS_TO_RUN) + printPots(currentPots));
             }
         }
 
-    // Now count up the indices of the pots with plants in them. That's our answer.
+        // Now count up the indices of the pots with plants in them. That's our answer.
         Integer potTotal = 0;
-        for(Pot pot:currentPots) {
-            if(pot.isHasPlant()) {
+        for (Pot pot : currentPots) {
+            if (pot.isHasPlant()) {
                 potTotal += pot.getPotId();
             }
         }
@@ -52,16 +52,43 @@ public class RunDay12 {
 
     public static void problem12B() {
     /*
-    Problem description
+    After 50 BILLION (50000000000) generations, what is the sum of the numbers of all pots which contain a plant?
     */
         System.out.println("=== DAY 12B ===");
 
+//        Integer GENERATIONS_TO_RUN = 50000000000;
+        Integer GENERATIONS_TO_RUN = 300;
+        ArrayList<Pot> currentPots = greenhouse.getPots(); // set the currentPots to be the initial state of the greenhouse
+        ArrayList<Rule> rules = greenhouse.getRules(); // use the greenhouse's rules.
+
+        if (DEBUG_PRINT_GREENHOUSE) {
+            System.out.println("S:\t" + addSpaces(currentPots, GENERATIONS_TO_RUN) + printPots(currentPots));
+        }
+
+        for (int generation = 1; generation <= GENERATIONS_TO_RUN; generation++) {
+            currentPots = GreenhouseService.calculateNextPots(currentPots, rules);
+
+            if (DEBUG_PRINT_GREENHOUSE) {
+                System.out.println(generation + ":\t" + addSpaces(currentPots, GENERATIONS_TO_RUN) + printPots(currentPots));
+            }
+        }
+
+        // Now count up the indices of the pots with plants in them. That's our answer.
+        Integer potTotal = 0;
+        for (Pot pot : currentPots) {
+            if (pot.isHasPlant()) {
+                potTotal += pot.getPotId();
+            }
+        }
+        System.out.println("Total sum of all occupied Pot IDs:\t" + potTotal);
+
+
     }
 
-    private static String addSpaces(ArrayList<Pot> pots) {
+    private static String addSpaces(ArrayList<Pot> pots, Integer generations) {
         Integer numPots = pots.size();
 
-        Integer numSpaces = 40 - (numPots - 100) / 2;
+        Integer numSpaces = generations * 2 - (numPots - 100) / 2 + 3;
         String output = String.join("", Collections.nCopies(numSpaces, SPACE));
         return output;
 
