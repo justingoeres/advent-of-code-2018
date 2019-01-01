d95d218c5374ffb8724d284affce360ba6a840b9
Justin Goeres <justin.goeres@gmail.com>
2019-01-01 10:06:08 -0500

    Day 16A Added CPU & initial opcode implementations

--- src/org/jgoeres/adventofcode/Day16/Memory.java
+++ src/org/jgoeres/adventofcode/Day16/Memory.java
@@ -3,12 +3,23 @@ package org.jgoeres.adventofcode.Day16;
 public class Memory {
     private int[] registers = new int[Register.values().length];
 
-    public int getRegisterValue(Register register){
+    public int getRegisterValue(Register register) {
         int value = registers[register.asInt()];
         return value;
     }
 
+    public int getRegisterValue(int registerAsInt) {
+        int value = registers[registerAsInt];
+        return value;
+    }
+
+
     public void setRegisterValue(Register register, int value) {
         registers[register.asInt()] = value;
     }
+
+    public void setRegisterValue(int registerAsInt, int value) {
+        registers[registerAsInt] = value;
+    }
+
 }
