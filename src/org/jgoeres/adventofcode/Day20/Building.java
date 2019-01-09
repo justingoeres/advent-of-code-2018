ddc63d2feaf9423b78e58a3bf6634e9aadaa221e
Justin Goeres <justin.goeres@gmail.com>
2019-01-09 00:04:03 -0500

    Day 20A Added immediate handling of finalized routes.

--- src/org/jgoeres/adventofcode/Day20/Building.java
+++ src/org/jgoeres/adventofcode/Day20/Building.java
@@ -6,6 +6,7 @@ import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
+import java.util.Stack;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -42,7 +43,7 @@ public class Building {
 
         String root = null;    // Match the root
         String remainder = null;
-        String parenString = null; // clip out the paren part
+        int routesProcessed = 0;
 
         currentList.add(line);
         while (true) {
@@ -69,18 +70,23 @@ public class Building {
                     nW.c.add(branch1);
                     nW.c.add(branch2);
                 } else {
-                    nW.c.add(map);
+//                    nW.c.add(map);
+                    // When we get a map route with NO parens, that one is done and we can run it!
+//                    System.out.println(map);
+                    runRoute(map);
+                    routesProcessed++;
                 }
             }
             // After we've got the new maps, swap the pointers.
-            if (nW.c.size() > cW.c.size()) { // If we added some new branches
-                // continue
-                swap(cW, nW);
-                nW.c.clear(); // Empty nextList after we swap.
-            } else {
+            if (nW.c.isEmpty()) { // If we have no more branched routes remaining to process
                 // We're done!
                 swap(cW, nW);
                 break;
+            } else {
+                System.out.println("Rooms:\t" + rooms.size() + "\tRoutes Finalized:\t" + routesProcessed + "\tTo Process:\t" + nW.c.size());
+                // continue
+                swap(cW, nW);
+                nW.c.clear(); // Empty nextList after we swap.
             }
         }
         System.out.println("here we are.");
@@ -90,82 +96,91 @@ public class Building {
 //        System.out.println(remainder);
 //
 //
-//        try (Scanner sc = new Scanner(new FileReader(pathToFile))) {
-//            sc.useDelimiter("");    // Scan the line character by character.
-//            Room currentRoom = null;
-//            while (sc.hasNext()) {
-//                Character nextDirection = sc.next().charAt(0);  // Get the next character.
-//
-//                Room newRoom = null;   // create a new room
-//                int newX = 0;
-//                int newY = 0;
-//
-//                switch (nextDirection) {
-//                    case '^':   // starting room
-//                        // Don't need to do anything, the default newX & newY will give us a room at 0,0.
-//                        break;
-//
-//                    case 'N':   // connect to the north.
-//                        newX = currentRoom.getX();
-//                        newY = currentRoom.getY() + 1; // y is positive to the north.
-//                        break;
-//
-//                    case 'E':   // connect to the east.
-//                        newX = currentRoom.getX() + 1; // x is positive to the east.
-//                        newY = currentRoom.getY();
-//                        break;
-//
-//                    case 'S':   // connect to the south.
-//                        newX = currentRoom.getX();
-//                        newY = currentRoom.getY() - 1; // y is negative to the south.
-//                        break;
-//
-//                    case 'W':   // connect to the west.
-//                        newX = currentRoom.getX() - 1; // x is negative to the west.
-//                        newY = currentRoom.getY();
-//                        break;
-//                    default:
-//                        System.out.println("unhandled character: " + nextDirection);
-//                }
-//
-//                // Look up the new room, or create it.
-//                if (!rooms.containsKey(XYtoKey(newX, newY))) {
-//                    // if this is a room we haven't seen before.
-//                    newRoom = new Room(newX, newY);
-//                    rooms.put(newRoom.toString(), newRoom); // add it to our map.
-//                } else {
-//                    // We've seen this room before!
-//                    newRoom = rooms.get(newRoom.toString());
-//                }
-//
-//                // Connect up to the new room.
-//                switch (nextDirection) {
-//                    case 'N':   // connect to the north.
-//                        currentRoom.connectToNorth(newRoom);
-//                        break;
-//
-//                    case 'E':   // connect to the east.
-//                        currentRoom.connectToEast(newRoom);
-//                        break;
-//
-//                    case 'S':   // connect to the south.
-//                        currentRoom.connectToSouth(newRoom);
-//                        break;
-//
-//                    case 'W':   // connect to the west.
-//                        currentRoom.connectToWest(newRoom);
-//                        break;
-//                    default:
-//                        System.out.println("unhandled character: " + nextDirection);
-//                }
-//
-//                currentRoom = newRoom;  // Move into the new room we just made.
-//
-//            }
-//
-//        } catch (Exception e) {
-//            System.out.println("Exception occurred: " + e.getMessage());
-//        }
+
+    }
+
+    private void runRoute(String route) {
+        try (Scanner sc = new Scanner(route)) {
+            sc.useDelimiter("");    // Scan the line character by character.
+            Room currentRoom = null;
+            while (sc.hasNext()) {
+                Character nextDirection = sc.next().charAt(0);  // Get the next character.
+
+                Room newRoom = null;   // create a new room
+                int newX = 0;
+                int newY = 0;
+
+                switch (nextDirection) {
+                    case '^':   // starting room
+                        // Don't need to do anything, the default newX & newY will give us a room at 0,0.
+                        break;
+
+                    case 'N':   // connect to the north.
+                        newX = currentRoom.getX();
+                        newY = currentRoom.getY() + 1; // y is positive to the north.
+                        break;
+
+                    case 'E':   // connect to the east.
+                        newX = currentRoom.getX() + 1; // x is positive to the east.
+                        newY = currentRoom.getY();
+                        break;
+
+                    case 'S':   // connect to the south.
+                        newX = currentRoom.getX();
+                        newY = currentRoom.getY() - 1; // y is negative to the south.
+                        break;
+
+                    case 'W':   // connect to the west.
+                        newX = currentRoom.getX() - 1; // x is negative to the west.
+                        newY = currentRoom.getY();
+                        break;
+
+                    case '$':   // End of the route.
+                        return; // We're done. Just return.
+
+                    default:
+                        System.out.println("unhandled character: " + nextDirection);
+                }
+
+                // Look up the new room, or create it.
+                newRoom = new Room(newX, newY);
+                if (!rooms.containsKey(XYtoKey(newX, newY))) {
+                    // if this is a room we haven't seen before.
+                    rooms.put(newRoom.toString(), newRoom); // add it to our map.
+                } else {
+                    // We've seen this room before!
+                    newRoom = rooms.get(newRoom.toString());
+                }
+
+                // Connect up to the new room.
+                switch (nextDirection) {
+                    case 'N':   // connect to the north.
+                        currentRoom.connectToNorth(newRoom);
+                        break;
+
+                    case 'E':   // connect to the east.
+                        currentRoom.connectToEast(newRoom);
+                        break;
+
+                    case 'S':   // connect to the south.
+                        currentRoom.connectToSouth(newRoom);
+                        break;
+
+                    case 'W':   // connect to the west.
+                        currentRoom.connectToWest(newRoom);
+                        break;
+                    default:
+                        // Skip unhandled characters (like ^ and $)
+                        //System.out.println("unhandled character: " + nextDirection);
+                }
+
+                currentRoom = newRoom;  // Move into the new room we just made.
+
+            }
+
+        } catch (Exception e) {
+            System.out.println("Exception occurred: " + e.getMessage());
+        }
     }
 
     private boolean findMatchingParen(String string, BranchStringInfo branchStringInfo) {
